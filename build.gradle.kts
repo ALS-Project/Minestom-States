bb3e4d2f8a2e374d599878d8764a58ec64ee5cf0
MrBretzel <axelolo68@gmail.com>
2022-09-19 02:33:57 +0200

    jitpack fix 2 ?

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,5 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-
     implementation("com.github.Minestom:Minestom:18c46481f4")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
